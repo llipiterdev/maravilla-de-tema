9f3d41acf01342d89140822bd17a815d6d1dfde0
spartan117 <42893609+camiloan@users.noreply.github.com>
2019-05-26 04:59:02 -0500

    Add files via upload
    Revisando los anexos de todos.

--- app.js
+++ app.js
@@ -7,22 +7,18 @@ var multer = require('multer');
 var cloudinary = require("cloudinary");
 var app_password = "123";
 var method_override = require("method-override");
-
-cloudinary.config({
-  cloud_name: "dzhaleuoj",
-  api_key: "966977923799895",
-  api_secret: "qz_iBU_YnWRlwjlbczSDgniwKlw"
-});
 var carga = multer({ dest: './uploads' });
 var app = express();
 var Suicide = null;
-mongoose.connect('mongodb://admin:UWNKVSWOJXVDHBYB@sl-us-south-1-portal.3.dblayer.com:19753/STD?authSource=admin&ssl=true', { useNewUrlParser: true })
- .then(() => {
- // Cuando se realiza la conexión, lanzamos este mensaje por consola
- Suicide = mongoose.model("SU", suicideSchemaJSON,'SU');
- console.log('La conexión a MongoDB se ha realizado correctamente!!');
- })
- .catch(err => console.log(err));
+
+mongoose.connect('mongodb://localhost:27017/Suicide_database', { useNewUrlParser: true })
+    .then(() => {
+        // Cuando se realiza la conexión, lanzamos este mensaje por consola
+        Suicide = mongoose.model("SU", suicideSchemaJSON, 'SU');
+        console.log('La conexión a MongoDB se ha realizado correctamente!!');
+    })
+    .catch(err => console.log(err));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(carga);
@@ -32,312 +28,301 @@ app.use(express.static("public_globe"));
 app.use(express.static("public"));
 app.use(express.static(__dirname));
 
-
 //Definir el schema de los datos
-var suicideSchemaJSON = new Schema ({
-  country: String,
-  year: Number,
-  sex: String,
-  age: String,
-  suicides_no: Number,
-  population: Number,
-  generation: String,
-  suicides_100kpop: Number,
-  country_year: String,
-  HDI_for_year: String,
-  gdp_for_year_$: String,
-  gdp_per_capita_$: String,
-  latitude: Number,
-  longitude: Number,
-  continent: String
+var suicideSchemaJSON = new Schema({
+    country: String,
+    year: Number,
+    sex: String,
+    age: String,
+    suicides_no: Number,
+    population: Number,
+    generation: String,
+    suicides_100kpop: Number,
+    country_year: String,
+    gdp_for_year_$: String,
+    gdp_per_capita_$: String,
+    latitude: Number,
+    longitude: Number,
+    continent: String
 });
 
-app.get("/", function (solicitud, respuesta) {
-  respuesta.render("index");
-  //res.end("Hola mundo");
+//Pagina de Inicio
+app.get("/", function(solicitud, respuesta) {
+    respuesta.render("index");
 });
 
-app.get('/dashboard', function(req, res){
-  res.sendFile(__dirname + 'myjs.js');
-});
-
-/*
-app.get("/menu", function (solicitud, respuesta) {
-  Product.find(function (error, documento) {
-    if (error) { console.log(error); }
-    respuesta.render("menu/index", { products: documento })
-  });
-});
-
-app.put("/menu/:id", function (solicitud, respuesta) {
-  if (solicitud.body.password == app_password) {
-    //console.log(solicitud.bodyParser);
-    var data = {
-      title: solicitud.body.title,
-      description: solicitud.body.description,
-      imageUrl: "data.png",
-      pricing: solicitud.body.pricing
-    };
-    Console.log(data);
-    if (solicitud.files.hasOwnProperty("image_avatar")) {
-      cloudinary.uploader.upload(solicitud.files.image_avatar.path,
-        function (result) {
-          data.imageUrl = result.url;
-          Product.update({ "_id": solicitud.params.id }, data, function (product) {
-            respuesta.redirect("/menu");
-          });
-        }
-      );
+//Pagina Cargar y Mostrar Datos
+app.post("/data", function(solicitud, respuesta) {
+    if (solicitud.body.password == app_password) {
+        Suicide.find(function(error, documento) {
+            if (error) { console.log(error); }
+            respuesta.render("data/index", { suicides: documento })
+        }).limit(20);
     } else {
-      Product.update({ "_id": solicitud.params.id }, data, function (product) {
-        respuesta.redirect("/menu");
-      });
+        respuesta.redirect("/");
     }
-  } else {
-    respuesta.redirect("/");
-  }
 });
-app.get("/menu/edit/:id", function (solicitud, respuesta) {
-  var id_producto = solicitud.params.id;
-  console.log(id_producto);
-  Product.findOne({ "_id": id_producto }, function (error, producto) {
-    console.log(producto);
-    respuesta.render("menu/edit", { product: producto });
-  });
+app.get("/data", function(solicitud, respuesta) {
+    respuesta.render("data/form");
 });
 
-app.post("/data", function (solicitud, respuesta) {
-  var data = {
-    country: "Colombia",
-    year: "2014",
-    sex: "female",
-    age: "55-74 years",
-    suicides_no: "21",
-    population: "1438935",
-    generation: "Boomers",
-    suicides_100kpop: "1.46",
-    country_year: "Colombia2014",
-    HDI_for_year_$: "0.675",
-    gdp_for_year_$: "63,067,077,179",
-    gdp_per_capita_$: "2309"
-  }
-  var suicide = new Suicide(data)
-  suicide.save(function (error) {
-    console.log(suicide);
-    respuesta.render("/data");
-  });
-});
-*/
-
-app.post("/data", function (solicitud, respuesta) {
-  if (solicitud.body.password == app_password) {
-    Suicide.find(function (error, documento) {
-      if (error) {
-        console.log(error);
-      }
-      console.log(documento.id);
-      respuesta.render("data/index", { suicides: documento })
-    }).where('country').equals('Colombia').limit(20);
-  } else {
-    respuesta.redirect("/");
-  }
-});
-
-
-app.get("/data", function (solicitud, respuesta) {
- respuesta.render("data/form");
- });
-/*
-app.post("/menu", function (solicitud, respuesta) {
-  if (solicitud.body.password == app_password) {
-    //console.log(solicitud.bodyParser);
-    var data = {
-      title: solicitud.body.title,
-      description: solicitud.body.description,
-      imageUrl: "data.png",
-      pricing: solicitud.body.pricing
-    }
-    var product = new Product(data);
-    //console.log(solicitud,files);
-    if (solicitud.files.hasOwnProperty("image_avatar")) {
-      cloudinary.uploader.upload(solicitud.files.image_avatar.path,
-        function (result) {
-          product.imageUrl = result.url;
-          product.save(function (err) {
-            console.log(product);
-            respuesta.redirect("/menu");
-          });
+//Pagina Nuevo Registro
+app.post("/data", function(solicitud, respuesta) {
+    if (solicitud.body.password == app_password) {
+        //console.log(solicitud.bodyParser);
+        var data = {
+            country: solicitud.body.country,
+            year: solicitud.body.year,
+            sex: solicitud.body.sex,
+            age: solicitud.body.age,
+            suicides_no: solicitud.body.suicides_no,
+            population: solicitud.body.population,
+            generation: solicitud.body.generation,
+            suicides_100kpop: solicitud.body.suicides_100kpop,
+            country_year: solicitud.body.country_year,
+            gdp_for_year_$: solicitud.body.gdp_for_year_$,
+            gdp_per_capita_$: solicitud.body.gdp_per_capita_$,
+            latitude: solicitud.body.latitude,
+            longitude: solicitud.body.longitude,
+            continent: solicitud.body.continent
         }
-      );
+        var suicide = new Suicide(data);
+        suicide.save(function(err) {
+            console.log(suicide);
+            respuesta.redirect("/");
+        });
     } else {
-      product.save(function (err) {
-        console.log(product);
-        respuesta.redirect("/menu");
-      });
+        respuesta.render("data/new");
     }
-  } else {
-    respuesta.render("menu/new");
-  }
 });
-app.get("/menu/new", function (solicitud, respuesta) {
-  respuesta.render("menu/new");
+app.get("/data/new", function(solicitud, respuesta) {
+    respuesta.render("data/new");
 });
 
-app.get("/menu/delete/:id", function (solicitud, respuesta) {
-  var id = solicitud.params.id;
-  Product.findOne({ "_id": id }, function (err, producto) {
-    respuesta.render("menu/delete", { producto: producto });
-  });
+//Pagina Editar
+app.put("/data/:id", function(solicitud, respuesta) {
+    if (solicitud.body.password == app_password) {
+        //console.log(solicitud.bodyParser);
+        var data = {
+            country: solicitud.body.country,
+            year: solicitud.body.year,
+            sex: solicitud.body.sex,
+            age: solicitud.body.age,
+            suicides_no: solicitud.body.suicides_no,
+            population: solicitud.body.population,
+            generation: solicitud.body.generation,
+            suicides_100kpop: solicitud.body.suicides_100kpop,
+            country_year: solicitud.body.country_year,
+            gdp_for_year_$: solicitud.body.gdp_for_year_$,
+            gdp_per_capita_$: solicitud.body.gdp_per_capita_$,
+            latitude: solicitud.body.latitude,
+            longitude: solicitud.body.longitude,
+            continent: solicitud.body.continent
+        };
+        Console.log(data);
+        Product.update({ "_id": solicitud.params.id }, data, function(product) {
+            respuesta.redirect("/");
+        });
+    } else {
+        respuesta.redirect("/");
+    }
 });
-app.delete("/menu/:id", function (solicitud, respuesta) {
-  var id = solicitud.params.id;
-  if (solicitud.body.password == app_password) {
-    Product.remove({ "_id": id }, function (err) {
-      if (err) { console.log(err); }
-      respuesta.redirect("/menu");
+
+app.get("/data/edit/:id", function(solicitud, respuesta) {
+    var id_suicide = solicitud.params.id;
+    console.log(id_suicide);
+    Suicide.findById(id_suicide, function(error, suicide) {
+        console.log(suicide);
+        respuesta.render("data/edit", { suicid: suicide });
     });
-  } else {
-    respuesta.redirect("/menu");
-
-  }
-});
-*/
-app.get("/acerca", function (solicitud, respuesta) {
-  respuesta.render("acerca/index");
-});
-app.get("/globe", function (solicitud, respuesta) {
-  respuesta.render("globe/index");
-});
-app.get("/dashboard", function (solicitud, respuesta) {
-  respuesta.render("dashboard/index");
 });
 
-app.get("/gender", function (solicitud, respuesta) {
-  console.log('Llego a consultar el conteo del genero')
-  mongoose.connection.db.collection('SU').aggregate([
-    {
-
-      $group : {
-        _id: '$sex',
-        total : {$sum:'$suicides_no'}
-    },
-    
-  }]).toArray(function(err, docs){
-
-    let totales = [];
-    if(err){
-
-      respuesta.send('Error en la consulta')
+//Pagina Delete
+app.get("/data/delete/:id", function(solicitud, respuesta) {
+    var id = solicitud.params.id;
+    Suicide.findById(id, function(err, documento) {
+        if (err) {
+            console.log(err);
+        }
+        respuesta.render("data/delete", { suicid: documento });
+    });
+});
+app.delete("/data/:id", function(solicitud, respuesta) {
+    var id = solicitud.params.id;
+    if (solicitud.body.password == app_password) {
+        Suicide.remove({ "_id": id }, function(err) {
+            if (err) { console.log(err); }
+            respuesta.redirect("/data");
+        });
+    } else {
+        respuesta.redirect("/data");
     }
-    for(let i = 0;i<docs.length;i++){
-      let actual = docs[i];
-
-      totales.push({label: actual._id,valor:actual.total})
-    }
-    respuesta.send({data : totales})
-
-  })
-
 });
 
+//Pagina Acerca del Tema
+app.get("/acerca", function(solicitud, respuesta) {
+    respuesta.render("acerca/index");
+});
 
-app.get("/generation", function (solicitud, respuesta) {
-console.log('Llego a consultar el conteo de la generación')
-mongoose.connection.db.collection('SU').aggregate([
-  {
+//Pagina WebGL Globe
+app.get("/globe", function(solicitud, respuesta) {
+    respuesta.render("globe/index");
+});
 
-    $group : {
-      _id: '$generation',
-      total : {$sum:'$suicides_no'}
-  },
-  
-}]).toArray(function(err, docs){
+//Pagina Dashboard
+app.get("/dashboard", function(solicitud, respuesta) {
+    respuesta.render("dashboard/index");
+});
 
-  let totales = [];
-  if(err){
+app.get('/dashboard', function(req, res) {
+    res.sendFile(__dirname + 'myjs.js');
+});
 
-    respuesta.send('Error en la consulta')
-  }
+//Consulta Genero
+app.get("/gender", function(solicitud, respuesta) {
+    console.log('Llego a consultar el conteo del genero')
+    mongoose.connection.db.collection('SU').aggregate([{
 
+        $group: {
+            _id: '$sex',
+            total: { $sum: '$suicides_no' }
+        },
 
-  for(let i = 0;i<docs.length;i++){
-    let actual = docs[i];
-   totales.push({label:actual._id, valor: actual.total})
-  }
+    }]).toArray(function(err, docs) {
 
-  respuesta.send({data : totales})
+        let totales = [];
+        if (err) {
 
-})
+            respuesta.send('Error en la consulta')
+        }
+        for (let i = 0; i < docs.length; i++) {
+            let actual = docs[i];
+
+            totales.push({ label: actual._id, valor: actual.total })
+        }
+        respuesta.send({ data: totales })
+
+    })
 
 });
 
-app.get("/age", function (solicitud, respuesta) {
-  console.log('Llego a consultar el conteo de la edad')
-  mongoose.connection.db.collection('SU').aggregate([
-    {
+//Consulta Edad
+app.get("/age", function(solicitud, respuesta) {
+    console.log('Llego a consultar el conteo de la edad')
+    mongoose.connection.db.collection('SU').aggregate([{
 
-      $group : {
-        _id: '$age',
-        total : {$sum:'$suicides_no'}
-      }
-    },
-    {
-    $sort : {
-      _id: 1
-    }
-  } 
-  ]).toArray(function(err, docs){
+            $group: {
+                _id: '$age',
+                total: { $sum: '$suicides_no' }
+            }
+        },
+        {
+            $sort: {
+                _id: 1
+            }
+        }
+    ]).toArray(function(err, docs) {
 
-    let totales = [];
-    if(err){
+        let totales = [];
+        if (err) {
 
-      respuesta.send('Error en la consulta')
-    }
-    for(let i = 0;i<docs.length;i++){
-      let actual = docs[i];
+            respuesta.send('Error en la consulta')
+        }
+        for (let i = 0; i < docs.length; i++) {
+            let actual = docs[i];
 
-      if(actual._id == '5-14 years'){
-        totales.unshift({label: actual._id.replace(" years",""), valor:actual.total})
-      } else {
-        totales.push({label: actual._id.replace(" years",""), valor:actual.total})
-      }
-    }
-    respuesta.send({data : totales})
+            if (actual._id == '5-14 years') {
+                totales.unshift({ label: actual._id.replace(" years", ""), valor: actual.total })
+            } else {
+                totales.push({ label: actual._id.replace(" years", ""), valor: actual.total })
+            }
+        }
+        respuesta.send({ data: totales })
 
-  })
+    })
 
 });
 
-app.get("/continent", function (solicitud, respuesta) {
-  console.log('Llego a consultar el conteo de la generación')
-  mongoose.connection.db.collection('SU').aggregate([
-    {
-  
-      $group : {
-        _id: '$continent',
-        total : {$sum:'$suicides_no'}
-    },
-    
-  }]).toArray(function(err, docs){
-  
-    let totales = [];
-    if(err){
-  
-      respuesta.send('Error en la consulta')
-    }
-  
-  
-    for(let i = 0;i<docs.length;i++){
-      let actual = docs[i];
-     totales.push({label:actual._id, valor: actual.total})
-    }
-  
-    respuesta.send({data : totales})
-  
-  })
-  
-  });
+//Consulta Generación
+app.get("/generation", function(solicitud, respuesta) {
+    console.log('Llego a consultar el conteo de la generación')
+    mongoose.connection.db.collection('SU').aggregate([{
 
+        $group: {
+            _id: '$generation',
+            total: { $sum: '$suicides_no' }
+        },
+
+    }]).toArray(function(err, docs) {
+
+        let totales = [];
+        if (err) {
+
+            respuesta.send('Error en la consulta')
+        }
+
+
+        for (let i = 0; i < docs.length; i++) {
+            let actual = docs[i];
+            totales.push({ label: actual._id, valor: actual.total })
+        }
+
+        respuesta.send({ data: totales })
+
+    })
+
+});
+
+//Consulta Continente
+app.get("/continent", function(solicitud, respuesta) {
+    console.log('Llego a consultar el conteo de la generación')
+    mongoose.connection.db.collection('SU').aggregate([{
+
+        $group: {
+            _id: '$continent',
+            total: { $sum: '$suicides_no' }
+        },
+
+    }]).toArray(function(err, docs) {
+
+        let totales = [];
+        if (err) {
+
+            respuesta.send('Error en la consulta')
+        }
+
+
+        for (let i = 0; i < docs.length; i++) {
+            let actual = docs[i];
+            totales.push({ label: actual._id, valor: actual.total })
+        }
+
+        respuesta.send({ data: totales })
+
+    })
+
+});
+
+//Consulta Etiquetas Paises
+app.get("/countryLabels", function(solicitud, respuesta) {
+    console.log('Llego a consultar los labels de cada Páis')
+    mongoose.connection.db.collection('SU').aggregate([{
+        $group: {
+            _id: '$country',
+        },
+    }]).toArray(function(err, docs) {
+        let totales = [];
+        if (err) {
+            respuesta.send('Error en la consulta')
+        }
+        for (let i = 0; i < docs.length; i++) {
+            let actual = docs[i];
+
+            totales.push({ label: actual._id })
+        }
+        respuesta.send({ data: totales })
+    })
+});
 
 //Puerto del servidor
 app.listen(8080);
