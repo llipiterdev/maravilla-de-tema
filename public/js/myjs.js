7887124efa430cc8152e76b6c98b86fa82ca0a81
llipiterdev <brayan.lml@hotmail.com>
2019-05-27 23:41:50 -0500

    implementacion graficas de puntos ok

--- public/js/myjs.js
+++ public/js/myjs.js
@@ -11,6 +11,9 @@ function drawChart(JsonUrl,idChart) {
         datasets: []
       },
       options: {
+        legend:{
+          position: 'bottom'
+        },
         responsive: false,
         scales: {
           yAxes: [{
@@ -64,7 +67,68 @@ function drawChart(JsonUrl,idChart) {
     return mychart;
   });
 }
+
+function DrawLineChart(JsonUrl, idChart) {
+  $.getJSON(JsonUrl, function (jsondata) {
+    var chartData = {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [{
+          label: 'Suicidos por año',
+          backgroundColor: 'red',
+          borderColor: 'black',
+          showLine: true,
+          data: [],
+          fill: false,
+        }]
+      },
+      options: {
+        responsive: false,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+              callback: function (label) { return label.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."); },
+            },
+          }],
+        },
+        title: {
+          display: true,
+          text: 'Suicidios por cada 100.000 habitantes'
+        },
+        tooltips: {
+          callbacks: {
+            label: function (tooltipItem, data) {
+              return tooltipItem.yLabel.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+            },
+          },
+        }
+      }
+    }
+
+    var label = jsondata.data.map(function (item) {
+      return item.label;
+    });
+    var value = jsondata.data.map(function (item) {
+      return item.valor;
+    });
+    var count = Object.keys(jsondata.data).length;
+
+    for (var i = 0; i < count; i++) {
+      chartData.data.labels.push(label[i]);
+      chartData.data.datasets[0].data.push(value[i]);
+    }
+    var ctx = document.getElementById(idChart).getContext('2d');
+    var myLineChart= new Chart(ctx, chartData);
+    console.log(chartData);
+    return myLineChart;
+  });
+
+}
 drawChart(UrlParse("gender"),"michart");
 drawChart(UrlParse("age"),"michart2");
 drawChart(UrlParse("generation"),"michart3");
 drawChart(UrlParse("continent"),"michart4");
+DrawLineChart(UrlParse("yearper100k"),"michart5");
+DrawLineChart(UrlParse("yearper100kco"),"michart6");
